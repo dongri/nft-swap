13108c9b5d762b44847e2a84b00a20fe733ddfbf
Dongri Jin <dongrify@gmail.com>
2023-04-03 11:42:39 +0900

    WIP

--- contracts/nft-swap.sol
+++ contracts/nft-swap.sol
@@ -20,6 +20,8 @@ contract NFTSwap is Initializable {
     event EventCancelRequest(address desiredERC721Address, uint256 desiredTokenId, address releaseERC721Address, uint256 releaseTokenId);
     event EventAcceptRequest(address desiredERC721Address, uint256 desiredTokenId, address releaseERC721Address, uint256 releaseTokenId);
 
+    uint256 constant public MAX_INT_TYPE = type(uint256).max;
+
     function initialize() public initializer {}
 
     function CreateRequest(address _desiredERC721Address, uint256 _desiredTokenId, address _releaseERC721Address, uint256 _releaseTokenId) public {
@@ -37,24 +39,26 @@ contract NFTSwap is Initializable {
     }
 
     function CancelRequest(address _desiredERC721Address, uint256 _desiredTokenId, address _releaseERC721Address, uint256 _releaseTokenId) public {
+        uint foundIndex = MAX_INT_TYPE;
         for (uint i = 0; i < swaps[msg.sender].length; i++) {
             SwapNFT memory swapNFT = swaps[msg.sender][i];
             if (swapNFT.desiredERC721Address == _desiredERC721Address && swapNFT.desiredTokenId == _desiredTokenId && swapNFT.releaseERC721Address == _releaseERC721Address && swapNFT.releaseTokenId == _releaseTokenId) {
-                delete swaps[msg.sender][i];
+                foundIndex = i;
                 break;
             }
         }
+        require(foundIndex != MAX_INT_TYPE, "Swap not found");
+        _removeSwapNFT(msg.sender, foundIndex);
         emit EventCancelRequest(_desiredERC721Address, _desiredTokenId, _releaseERC721Address, _releaseTokenId);
     }
 
     function AcceptRequest(address _desiredERC721Address, uint256 _desiredTokenId, address _releaseERC721Address, uint256 _releaseTokenId) public {
         ERC721 releaseContract = ERC721(_releaseERC721Address);
         address releaseOwner = releaseContract.ownerOf(_releaseTokenId);
-        bool isSwapFound = false;
+        uint foundIndex = MAX_INT_TYPE;
         for (uint i = 0; i < swaps[releaseOwner].length; i++) {
             SwapNFT memory swapNFT = swaps[releaseOwner][i];
             if (swapNFT.desiredERC721Address == _desiredERC721Address && swapNFT.desiredTokenId == _desiredTokenId && swapNFT.releaseERC721Address == _releaseERC721Address && swapNFT.releaseTokenId == _releaseTokenId) {
-                address releaseERC721Address = swapNFT.releaseERC721Address;
 
                 ERC721 desiredContract = ERC721(_desiredERC721Address);
                 address desiredOwner = desiredContract.ownerOf(_desiredTokenId);
@@ -62,25 +66,30 @@ contract NFTSwap is Initializable {
                 uint256 desiredTokenId = swapNFT.desiredTokenId;
                 uint256 releaseTokenId = swapNFT.releaseTokenId;
 
-                require(desiredContract.getApproved(desiredTokenId) == address(this), "Token is not approved for swap");
-                require(releaseContract.getApproved(releaseTokenId) == address(this), "Token is not approved for swap");
+                require(desiredContract.getApproved(desiredTokenId) == address(this), "Desired Token is not approved for swap");
+                require(releaseContract.getApproved(releaseTokenId) == address(this), "Release Token is not approved for swap");
 
                 desiredContract.safeTransferFrom(desiredOwner, releaseOwner, desiredTokenId);
                 releaseContract.safeTransferFrom(releaseOwner, desiredOwner, releaseTokenId);
 
-                delete swaps[msg.sender][i];
-
-                isSwapFound = true;
-
-                emit EventAcceptRequest(_desiredERC721Address, desiredTokenId, releaseERC721Address, releaseTokenId);
+                foundIndex = i;
                 break;
             }
         }
-        require(isSwapFound, "Swap not found");
+        require(foundIndex != MAX_INT_TYPE, "Swap not found");
+        _removeSwapNFT(releaseOwner, foundIndex);
+        emit EventAcceptRequest(_desiredERC721Address, _desiredTokenId, _releaseERC721Address, _releaseTokenId);
     }
 
     function GetSwaps(address _owner) public view returns (SwapNFT[] memory) {
         return swaps[_owner];
     }
 
+    function _removeSwapNFT(address _owner, uint256 _index) private {
+        if (_index >= swaps[_owner].length) return;
+        for (uint i = _index; i < swaps[_owner].length - 1; i++){
+            swaps[_owner][i] = swaps[_owner][i+1];
+        }
+        swaps[_owner].pop();
+    }
 }
--- test/nft-swap.js
+++ test/nft-swap.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers, upgrades } = require("hardhat");
 
-describe("NFT", function () {
+describe("NFTSwap", function () {
 
   let nft1;
   let nft2;
@@ -47,78 +47,75 @@ describe("NFT", function () {
     expect(nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
 
   });
-  
 
-  // it("should create a swap request-success", async function () {
-    
-  //   const nft1ContractWithAdd1 = nft1.connect(addr1);
-  //   await nft1ContractWithAdd1.mint(addr1.address, 1);
-  //   await nft1ContractWithAdd1.approve(swap.address, 1);
+  it("should cancel a swap request", async function () {
+    const nftSwapContractWithAdd1 = await swap.connect(addr1);
 
-  //   const nft2ContractWithAdd2 = nft2.connect(addr2);
-  //   await nft2ContractWithAdd2.mint(addr2.address, 2);
-  //   await nft2ContractWithAdd2.approve(swap.address, 2);
+    const nft1ContractWithAdd1 = await nft1.connect(addr1);
+    await nft1ContractWithAdd1.mint(addr1.address, 1);
+    await nft1ContractWithAdd1.approve(swap.address, 1);
 
-  
-  //   const nftSwapContractWithAdd1 = await swap.connect(addr1);
-  //   await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 1);
+    const nft2ContractWithAdd2 = await nft2.connect(addr2);
+    await nft2ContractWithAdd2.mint(addr2.address, 2);
 
-  //   const swaps = await swap.GetSwaps(addr1.address);
-  //   expect(swaps.length).to.equal(1);
-  // });
-  
+    expect(await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+    const swapsBefore = await nftSwapContractWithAdd1.GetSwaps(addr1.address);
+    expect(swapsBefore.length).to.equal(1);
 
-  // it("should cancel a swap request", async function () {
-  //   const desiredERC721 = await ethers.getContractFactory("MyERC721");
-  //   const desiredToken = await desiredERC721.deploy();
-  //   await desiredToken.deployed();
-  
-  //   const releaseERC721 = await ethers.getContractFactory("MyERC721");
-  //   const releaseToken = await releaseERC721.deploy();
-  //   await releaseToken.deployed();
-  
-  //   await releaseToken.mint(owner.address, 1);
-  //   await releaseToken.approve(swap.address, 1);
-  
-  //   await swap.CreateRequest(desiredToken.address, 1, releaseToken.address, 1);
-  
-  //   const swapsBefore = await swap.GetSwaps(owner.address);
-  //   expect(swapsBefore.length).to.equal(1);
-  
-  //   await swap.CancelRequest(desiredToken.address, 1, releaseToken.address, 1);
-  
-  //   const swapsAfter = await swap.GetSwaps(owner.address);
-  //   expect(swapsAfter.length).to.equal(0);
-  // });
+    await expect(nftSwapContractWithAdd1.CancelRequest(nft2.address, 2, nft1.address, 2)).to.be.revertedWith("Swap not found");
+    await expect(nftSwapContractWithAdd1.CancelRequest(nft2.address, 1, nft1.address, 1)).to.be.revertedWith("Swap not found");
 
-  // it("should swap two NFTs", async function () {
-  //   const desiredERC721 = await ethers.getContractFactory("MyERC721");
-  //   const desiredToken = await desiredERC721.deploy();
-  //   await desiredToken.deployed();
+    expect(await nftSwapContractWithAdd1.CancelRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+    const swapsAfter = await nftSwapContractWithAdd1.GetSwaps(addr1.address);
+    expect(swapsAfter.length).to.equal(0);
+
+    await nft1ContractWithAdd1.mint(addr1.address, 2);
+    await nft1ContractWithAdd1.approve(swap.address, 2);
+    await nft1ContractWithAdd1.mint(addr1.address, 3);
+    await nft1ContractWithAdd1.approve(swap.address, 3);
+
+    expect(await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+    expect(await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 2)).to.be.ok;
+    expect(await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 3)).to.be.ok;
+    let swaps = await nftSwapContractWithAdd1.GetSwaps(addr1.address);
+    expect(swaps.length).to.equal(3);
+
+    expect(await nftSwapContractWithAdd1.CancelRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+    swaps = await nftSwapContractWithAdd1.GetSwaps(addr1.address);
+    expect(swaps.length).to.equal(2);
   
-  //   const releaseERC721 = await ethers.getContractFactory("MyERC721");
-  //   const releaseToken = await releaseERC721.deploy();
-  //   await releaseToken.deployed();
-  
-  //   await desiredToken.mint(addr1.address, 1);
-  //   await desiredToken.approve(swap.address, 1);
-  
-  //   await releaseToken.mint(owner.address, 1);
-  //   await releaseToken.approve(swap.address, 1);
-  
-  //   await swap.CreateRequest(desiredToken.address, 1, releaseToken.address, 1);
-  
-  //   await swap.AcceptRequest(desiredToken.address, 1, releaseToken.address, 1);
-  
-  //   const desiredOwner = await desiredToken.ownerOf(1);
-  //   const releaseOwner = await releaseToken.ownerOf(1);
-  
-  //   expect(desiredOwner).to.equal(owner.address);
-  //   expect(releaseOwner).to.equal(addr1.address);
-  
-  //   const swapsAfter = await swap.GetSwaps(owner.address);
-  //   expect(swapsAfter.length).to.equal(0);
-  // });
+  });
+
+  it("should accept a swap request", async function () {
+    const nftSwapContractWithAdd1 = await swap.connect(addr1);
+
+    const nft1ContractWithAdd1 = await nft1.connect(addr1);
+    await nft1ContractWithAdd1.mint(addr1.address, 1);
+    await nft1ContractWithAdd1.approve(swap.address, 1);
+
+    const nft2ContractWithAdd2 = await nft2.connect(addr2);
+    await nft2ContractWithAdd2.mint(addr2.address, 2);
+
+    expect(await nftSwapContractWithAdd1.CreateRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+    const swapsBefore = await nftSwapContractWithAdd1.GetSwaps(addr1.address);
+    expect(swapsBefore.length).to.equal(1);
+
+    const nftSwapContractWithAdd2 = await swap.connect(addr2);
+    await expect(nftSwapContractWithAdd2.AcceptRequest(nft2.address, 2, nft1.address, 1)).to.be.revertedWith("Desired Token is not approved for swap" );
+
+    await nft2ContractWithAdd2.approve(swap.address, 2);
+
+    await nft1ContractWithAdd1.approve(addr3.address, 1);
+
+    await expect(nftSwapContractWithAdd2.AcceptRequest(nft2.address, 2, nft1.address, 1)).to.be.revertedWith("Release Token is not approved for swap" );
+
+    await nft1ContractWithAdd1.approve(swap.address, 1);
+
+    expect(await nftSwapContractWithAdd2.AcceptRequest(nft2.address, 2, nft1.address, 1)).to.be.ok;
+
+    await expect(nftSwapContractWithAdd2.AcceptRequest(nft2.address, 1, nft1.address, 1)).to.be.revertedWith("Swap not found" );
+
+  });
   
 
 });
